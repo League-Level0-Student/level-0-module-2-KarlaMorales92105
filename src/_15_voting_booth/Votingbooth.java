bd8d5358b42f2678f4f0397124bca819a42b3114
KarlaMorales92105 <55598718+KarlaMorales92105@users.noreply.github.com>
2020-02-20 13:34:54 -0800

    Add files via upload

---
+++ src/_15_voting_booth/Votingbooth.java
@@ -0,0 +1,30 @@
+package _15_voting_booth;
+
+import javax.swing.JOptionPane;
+
+public class Votingbooth {
+	public static void main(String[] args) {
+		
+		String age= JOptionPane.showInputDialog("What is your age?");
+		
+		int answer= Integer.parseInt(age);
+		
+		
+	if(answer > 18) {
+		JOptionPane.showMessageDialog(null, "Who o you think the ");
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
